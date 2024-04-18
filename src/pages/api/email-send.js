bf82a89df6a7c4aaf18da6343b26f1d65263412e
null <malek0x1@gmail.com>
2024-04-18 16:11:36 +0300

    fix sending email from domain

--- src/pages/api/email-send.js
+++ src/pages/api/email-send.js
@@ -1,26 +1,66 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+import axios from "axios";
 
-export default async (req, res) => {
-    if (req.method === 'POST' && req.body.message) {
-        const { message } = req.body;
-        try {
-            const { data, error } = await resend.emails.send({
-                from: 'onboarding@resend.dev',
-                to: 'malek0x1@gmail.com',
-                subject: 'Email From my Porfolio',
-                react: <div>{atob(message)}</div>,
-            });
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-            if (error) {
-                res.status(400).json({ error });
+// export default async (req, res) => {
+//     if (req.method === 'POST' && req.body.message) {
+//         const { message } = req.body;
+//         try {
+//             const { data, error } = await resend.emails.send({
+//                 from: 'onboarding@resend.dev',
+//                 to: 'malek0x1@gmail.com',
+//                 subject: 'Email From my Porfolio',
+//                 react: <div>{atob(message)}</div>,
+//             });
+
+//             if (error) {
+//                 res.status(400).json({ error });
+//             }
+
+//             res.status(200).json({ data });
+//         } catch (error) {
+//             res.status(400).json({ error });
+//         }
+//     }
+//     res.status(400).json({ error: "method not supported" });
+// }
+
+const sendEmailBackend = async (msg) => {
+    try {
+        const response = await axios.post("https://api.resend.com/emails",
+            {
+                "from": 'onboarding@resend.dev',//fromEmail,
+                "to": "malek0x1@gmail.com",
+                "subject": 'Email From my Porfolio',
+                "html": atob(msg)
+            },
+            {
+                headers: {
+                    'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
+                    'Content-Type': 'application/json'
+                }
             }
+        )
+        return response
+    }
+    catch (err) {
+        throw new Error("Error");
+    }
+}
 
-            res.status(200).json({ data });
+
+export default async function handler(req, res) {
+    if (req.method === 'POST' && req.body.message) {
+        try {
+            const { message } = req.body;
+            await sendEmailBackend(message)
+            return res.status(200).json({ status: "successs" })
         } catch (error) {
-            res.status(400).json({ error });
+            console.error('Error generating token:', error);
+            return res.status(500).json({ error: 'Internal server error' });
         }
     }
-    res.status(400).json({ error: "method not supported" });
+    return res.status(400).json({ error: 'Email is required' });
 }
