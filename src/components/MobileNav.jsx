462db0dbeb337f3f6d6dfb324fa0ab20794faff2
null <malek0x1@gmail.com>
2024-04-18 19:03:07 +0300

    cleaning

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,31 +1,46 @@
 import { NAVBAR_DATA } from "@/other/data"
 import { motion } from "framer-motion"
 import Link from "next/link"
-import { RemoveScroll } from "react-remove-scroll"
+import { useEffect } from "react"
 
-const MobileNav = ({ setIsOpen }) => {
+const MobileNav = ({ isOpen, setIsOpen }) => {
+
+    useEffect(() => {
+        const handleScrollLock = () => {
+            if (isOpen) {
+                document.body.style.overflow = 'hidden';
+            } else {
+                document.body.style.overflow = 'auto';
+            }
+        };
+
+        handleScrollLock();
+
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [isOpen]);
 
 
     return (
         <motion.div
-            initial={{ scale: 0, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            exit={{ opacity: 0 }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0, }}
             className="mobile-nav"
+            layout="position"
         >
-            <RemoveScroll>
-                <div className="pt-6 cursor-pointer">
-                    <div className="flex flex-col gap-4 p-4 ">
-                        {NAVBAR_DATA.map(item => (
-                            <div key={item.id} className="p-2 ">
-                                <Link onClick={() => { setIsOpen(false) }} href={item.href}>
-                                    {item.title}
-                                </Link>
-                            </div>
-                        ))}
-                    </div>
+            <div className="pt-6 cursor-pointer">
+                <div className="flex flex-col gap-4 p-4 ">
+                    {NAVBAR_DATA.map(item => (
+                        <div key={item.id} className="p-2 ">
+                            <Link onClick={() => { setIsOpen(false) }} href={item.href}>
+                                {item.title}
+                            </Link>
+                        </div>
+                    ))}
                 </div>
-            </RemoveScroll>
+            </div>
         </motion.div>
     )
 }
